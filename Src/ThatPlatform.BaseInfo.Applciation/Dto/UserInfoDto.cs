31fd488ef07cfe0ffb8974cfd9d369206e2be3e7
Pengxi <pengxi1520@outlook.com>
2022-03-22 23:42:28 +0800

    porject: Dto & Enum

---
+++ Src/ThatPlatform.BaseInfo.Applciation/Dto/UserInfoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThatPlatform.BaseInfo.Applciation.Dto
+{
+    internal class UserInfoInputDto
+    {
+    }
+
+
+    internal class UserInfoOutputDto
+    {
+    }
+}
