b08e9cd0f08c886169ee38ecf5216d6047fdaac6
pengxi <pengxi1520@outlook.com>
2023-11-15 22:52:03 +0800

    refactor: update to .NET 8.0

--- Src/Hosting/Tpf.Ugly.Api/Startup.cs
+++ Src/Hosting/Tpf.Ugly.Api/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using ProtoBuf.Grpc.Server;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Tpf.Grpc.Server;
 using Tpf.Middleware.Middlewares;
 
