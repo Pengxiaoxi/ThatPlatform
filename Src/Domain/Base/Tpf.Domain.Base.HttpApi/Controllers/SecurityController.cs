74d9c91a4bcb762b673914b623c5a48ae32e0aef
pengxi <pengxi1520@outlook.com>
2024-04-02 10:52:14 +0800

    feat: SecurityController

--- Src/Domain/Base/Tpf.Domain.Base.HttpApi/Controllers/SecurityController.cs
+++ Src/Domain/Base/Tpf.Domain.Base.HttpApi/Controllers/SecurityController.cs
@@ -1,18 +1,41 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tpf.Security;
+using Tpf.Utils;
 
 namespace Tpf.Domain.Base.HttpApi.Controllers
 {
     /// <summary>
-    /// 加解密接口 [AllowAnonymous]
+    /// 加解密接口
     /// </summary>
-    [AllowAnonymous]
+    //[AllowAnonymous]
     public class SecurityController : BaseApiController
     {
+        /// <summary>
+        /// 1、AESEncrypt
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<string> AESEncrypt([FromBody] string content)
+        {
+            var result = AESHelper.Encrypt(content, ConfigHelper.GetSecurityKey16());
+
+            return await Task.FromResult(result);
+        }
+
+        /// <summary>
+        /// 2、AESDecrypt
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<string> AESDecrypt([FromBody] string content)
+        {
+            var result = AESHelper.Decrypt(content, ConfigHelper.GetSecurityKey16());
+
+            return await Task.FromResult(result);
+        }
 
     }
 }
