c740968a2324432f586508d9a9369d1773b51345
pengxi <pengxi1520@outlook.com>
2024-04-14 21:44:18 +0800

    feat: Tpf.Caching.CSRedisCore
    基于 CSRedisCore

---
+++ src/Component/Tpf.Caching.CSRedisCore/CSRedisCoreExtenisons.cs
@@ -0,0 +1,43 @@
+﻿using CSRedis;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Tpf.Utils;
+using Tpf.Utils.Check;
+
+namespace Tpf.Caching.CSRedisCore
+{
+    public static class CSRedisCoreExtenisons
+    {
+        /// <summary>
+        /// CSRedisCore
+        /// https://github.com/2881099/csredis
+        /// IDistributedCache: https://github.com/2881099/csredis/blob/master/src/Microsoft.Extensions.Caching.CSRedis/README.md
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddTpfCSRedisCore(this IHostApplicationBuilder builder)
+        {
+            var redisOptions = ConfigHelper.GetOptions<RedisOptions>();
+
+            Check.NotNull(redisOptions, nameof(redisOptions));
+
+            CSRedisClient csredis = null;
+            if (!string.IsNullOrWhiteSpace(redisOptions.Default))
+            {
+                csredis = new CSRedis.CSRedisClient(redisOptions.Default);
+            }
+            else if (redisOptions.Cluser != null && redisOptions.Cluser.Length > 0)
+            {
+                csredis = new CSRedis.CSRedisClient(null, redisOptions.Cluser);
+            }
+
+            Check.NotNull(csredis, nameof(csredis), "RedisOptions not be null.");
+
+            RedisHelper.Initialization(csredis);
+
+            // IDistributedCache
+            //builder.Services.AddSingleton<IDistributedCache>(new Microsoft.Extensions.Caching.Redis.CSRedisCache(RedisHelper.Instance));
+
+        }
+    }
+}
---
+++ src/Shared/Utils/Tpf.Utils/Check/Check.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Tpf.Utils.Check
+{
+    /// <summary>
+    /// Check
+    /// </summary>
+    public static class Check
+    {
+        public static T NotNull<T>(
+            T value,
+            [NotNull] string parameterName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return value;
+        }
+
+        public static T NotNull<T>(
+            T value,
+            [NotNull] string parameterName,
+            string message)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName, message);
+            }
+
+            return value;
+        }
+
+
+    }
+}
