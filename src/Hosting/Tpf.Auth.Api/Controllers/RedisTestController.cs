a36b36b83580e6e7aaad30db665c779d5529f054
pengxi <pengxi1520@outlook.com>
2024-07-15 11:23:06 +0800

    fix: Redis List Test

---
+++ src/Hosting/Tpf.Auth.Api/Controllers/RedisTestController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+using Tpf.Domain.Base.HttpApi;
+
+namespace Tpf.Auth.Api.Controllers
+{
+    /// <summary>
+    /// Redis Test
+    /// </summary>
+    [AllowAnonymous]
+    public class RedisTestController : BaseApiController
+    {
+        private const string ListKey = "my_list";
+
+
+        [HttpPost]
+        public async Task<bool> ListLeftPush([FromBody] string value)
+        {
+            //Task.Run(async () =>
+            //{
+            //    for (int i = 1; i <= 100; i++)
+            //    {
+            //        await RedisHelper.LPushAsync(ListKey, $"{i}");
+
+            //        Console.WriteLine(i);
+
+            //        //Thread.Sleep(1 * 1000);
+            //    }
+            //});
+
+
+            for (int i = 1; i <= 100; i++)
+            {
+                await RedisHelper.LPushAsync(ListKey, $"{i}");
+
+                Console.WriteLine(i);
+
+                //Thread.Sleep(1 * 1000);
+            }
+
+            //var conList = new ConcurrentBag<int>();
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    conList.Add(i);
+            //}
+
+            //Parallel.ForEach(conList, new ParallelOptions() { MaxDegreeOfParallelism = 5 }, async (x) => 
+            //{
+            //    await RedisHelper.LPushAsync(ListKey, $"{x}");
+
+            //    Console.WriteLine(x);
+            //});
+
+            return true;
+        }
+    }
+}
---
+++ src/Hosting/Tpf.Platform.Api/Controllers/RedisTestController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tpf.Domain.Base.HttpApi;
+using Tpf.Utils;
+
+namespace Tpf.Platform.Api.Controllers
+{
+    /// <summary>
+    /// Redis Test
+    /// </summary>
+    [AllowAnonymous]
+    public class RedisTestController : BaseApiController
+    {
+        private const string ListKey = "my_list";
+
+        /// <summary>
+        /// Redis List Test
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<bool> ListRightPop()
+        {
+            Task.Run(async () =>
+            {
+                int count = 0;
+                while (true) 
+                {
+                    //Thread.Sleep(1 * 1000);
+
+                    var rightValue = await RedisHelper.RPopAsync(ListKey);
+
+                    if (!string.IsNullOrEmpty(rightValue))
+                    {
+                        Console.WriteLine(rightValue);
+
+                        count++;
+
+                        ConsoleHelper.WriteSuccessLine($"总计：{count}");
+                    }
+
+                };
+            });
+
+            return true;
+        }
+
+    }
+}
