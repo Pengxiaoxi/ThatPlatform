bfaa553552cc1c5c40674501cf18dc302a4f5b00
pengxi <pengxi1520@outlook.com>
2024-07-24 15:10:18 +0800

    feat: RabbitMQ RpcClient/RpcServer

--- src/Component/Tpf.RabbitMQ/RpcClient.cs
+++ src/Component/Tpf.RabbitMQ/RpcClient.cs
@@ -78,26 +78,3 @@ namespace Tpf.RabbitMQ
 
 
 }
-
-//public class Rpc
-//{
-//    public static async Task Main(string[] args)
-//    {
-//        Console.WriteLine("RPC Client");
-//        string n = args.Length > 0 ? args[0] : "30";
-//        await InvokeAsync(n);
-
-//        Console.WriteLine(" Press [enter] to exit.");
-//        Console.ReadLine();
-//    }
-
-//    private static async Task InvokeAsync(string n)
-//    {
-//        using var rpcClient = new RpcClient();
-
-//        Console.WriteLine(" [x] Requesting fib({0})", n);
-//        var response = await rpcClient.CallAsync(n);
-//        Console.WriteLine(" [.] Got '{0}'", response);
-//    }
-
-//}
